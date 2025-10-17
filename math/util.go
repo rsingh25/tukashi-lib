9f9f5e3dc8c14cc1f3a6867a49cb8f852a4d3eb3
Raghvendra <raghvendra.pratap.singh@gmail.com>
2025-10-17 13:27:03 +0000

    added

---
+++ math/util.go
@@ -0,0 +1,6 @@
+package math
+
+// Double returns the input integer multiplied by two
+func Double(x int) int {
+	return x * 2
+}
--- util/util.go
+++ util/util.go
@@ -1,14 +1,8 @@
-// utils.go
-package mymodule
+package util
 
 import "fmt"
 
 // PrintGreeting prints a standardized greeting message
 func PrintGreeting(name string) {
 	fmt.Printf("Hello, %s! Welcome to the custom module.\n", name)
-}
-
-// Double returns the input integer multiplied by two
-func Double(x int) int {
-	return x * 2
 }
